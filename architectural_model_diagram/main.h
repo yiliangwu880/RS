129e26ad7fe2c5183255e1dbe8d6d2a4a641449e
yiliangwu880 <30390012@qq.com>
2019-11-28 11:18:37 +0800

    加入组件

--- architectural_model_diagram/main.h
+++ architectural_model_diagram/main.h
@@ -2,75 +2,36 @@
 
 //用类图描述框架结构
 
-class protobuf
-{
-
-};
-class libevent
-{
-
-};
+class boost {};
+class protobuf3 {};
+class mysql_connector : public boost{};
+class libevent {};
+class LuaBridge {};
+class lua {};
 
 ////////////////////////////////////////////////
-class libevent_cpp : public libevent
-{
+class cpp_cfg {};
 
-};
+////////////////////////////////////////////////
+class libevent_cpp : public libevent {};
+class svr_util {};
 
-class svr_util
-{
+////////////////////////////////////////////////
+class acc_proto {};
+class access_svr : public libevent_cpp, public svr_util, public acc_proto {};
+class acc_driver : public libevent_cpp, public svr_util, public acc_proto {};
 
-};
+////////////////////////////////////////////////
+class mf_proto {};
+class mf_driver : public mf_proto, public libevent_cpp {};
+class mf_svr : public libevent_cpp, public svr_util, public mf_proto {};
 
-//让CPP LUA交换更简化
-class lua_mgr
-{
+////////////////////////////////////////////////
+class db_proxy_svr : public libevent_cpp, public svr_util, public protobuf3, public mysql_connector {};
+class db_driver : public libevent_cpp, public svr_util, public protobuf3 {};
 
-};
+////////////////////////////////////////////////
+class cpp_lua_mgr : public LuaBridge, public lua {};
 
-class acc_proto
-{
-
-};
-
-//access_svr
-class access_svr : public libevent_cpp, public svr_util, public acc_proto
-{
-
-};
-
-//业务服务器接入网关驱动
-//access connect driver  
-class acc_driver : public libevent_cpp, public svr_util, public acc_proto
-{
-
-};
-
-class login_svr : public libevent_cpp, public svr_util, public acc_con_dev
-{
-
-};
-
-//proto协议交换，读取任意DB。 mysql, redis, mongodb
-class db_proxy_svr : public libevent_cpp, public svr_util, public protobuf
-{
-
-};
-
-
-class mf_proto
-{
-
-};
-
-class mf_driver : public mf_proto, public libevent_cpp
-{
-
-};
-
-
-class mf_svr : public libevent_cpp, public svr_util, public mf_proto
-{
-
-};
-
+////////////////////////////////////////////////
+class simple_web_svr : public libevent_cpp, public svr_util {};
